f423a85e334b22b32a7421f4bb2357ca3940b94e
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-03-15 00:02:40 +0100

    Update: add test for clamp function

---
+++ __tests__/clamp/clamp.test.ts
@@ -0,0 +1,20 @@
+import {clamp} from "@/clamp/clamp";
+
+
+describe("clamp", () => {
+	it("should return min if value is less than min", () => {
+		expect(clamp(0, 1, -1)).toBe(0);
+	});
+
+	it("should return max if value is greater than max", () => {
+		expect(clamp(0, 1, 2)).toBe(1);
+	});
+
+	it("should return the value if it is between min and max", () => {
+		expect(clamp(0, 1, 0.5)).toBe(0.5);
+	});
+
+	it("should throw an error if min is greater than max", () => {
+		expect(() => clamp(1, 0, 0)).toThrow();
+	});
+});
---
+++ src/clamp/clamp.ts
@@ -0,0 +1,24 @@
+type Clamp = (min: number, max: number, value: number) => number;
+
+/**
+	This function returns a value that is constrained between a specified minimum and maximum value.
+	If the value is less than the minimum value, it returns the minimum value.
+	If the value is greater than the maximum value, it returns the maximum value.
+	@function
+	@param {number} min - The minimum allowed value.
+	@param {number} max - The maximum allowed value.
+	@param {number} value - The value to check.
+	@throws {Error} If the minimum value is greater than the maximum value.
+	@throws {Error} If the value is less than the minimum value.
+	@throws {Error} If the value is greater than the maximum value.
+	@returns {number} The checked and possibly corrected value.
+ */
+
+export const clamp: Clamp = (min, max, value) => {
+	if (min > max) throw new Error('min must be less than or equal to max');
+	return Math.min(Math.max(value, min), max);
+};
+
+
+
+
