7f23df9de48631331e7a898a55322ef287538d45
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-03-17 20:21:44 +0100

    Update: package.json

--- build/src/clamp/clamp.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clamp = void 0;
-var clamp = function (min, max, value) {
-    if (min > max)
-        throw new Error('min must be less than or equal to max');
-    return Math.min(Math.max(value, min), max);
-};
-exports.clamp = clamp;
